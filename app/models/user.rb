3fbee47420feeab5b63f72fb008a7c9c68fc1637
kobegoro0930 <62911575+kobegoro@users.noreply.github.com>
2020-07-17 20:32:52 +0900

    Create DB

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :name, presence: true, uniqueness: true
+  validates :email, uniqueness: true
+  validates :password,
+  format: {
+    with: /\A[a-zA-Z0-9]+\z/
+  }
 end
--- db/migrate/20200716103615_devise_create_users.rb
+++ db/migrate/20200716103615_devise_create_users.rb
@@ -4,8 +4,10 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
       ## Database authenticatable
-      t.string :email,              null: false, default: ""
+      t.string :name,               null: false, unique: true
+      t.string :email,              null: false, unique: true, default: ""
       t.string :encrypted_password, null: false, default: ""
+      t.string :image
 
       ## Recoverable
       t.string   :reset_password_token
@@ -36,6 +38,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
       t.timestamps null: false
     end
 
+    add_index :users, :name,                 unique: true
     add_index :users, :email,                unique: true
     add_index :users, :reset_password_token, unique: true
     # add_index :users, :confirmation_token,   unique: true
