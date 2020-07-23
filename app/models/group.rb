c82ea1e4c23a1e1171a1a98f103e2bf105e523a8
kobegoro0930 <62911575+kobegoro@users.noreply.github.com>
2020-07-23 12:45:48 +0900

    Configuration association

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,2 +1,9 @@
 class Group < ApplicationRecord
+  belongs_to :user
+  has_many :saves
+  has_many :groups_tags
+  has_many :tags, throgh: :groups_tags
+  has_many :images
+  has_many :comments
+  has_many :wents
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_16_103615) do
+ActiveRecord::Schema.define(version: 2020_07_23_022325) do
+
+  create_table "groups", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "user_id"
+    t.string "name", null: false
+    t.string "catchphrase", null: false
+    t.string "title", null: false
+    t.string "text", null: false
+    t.string "area", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_groups_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -27,4 +39,5 @@ ActiveRecord::Schema.define(version: 2020_07_16_103615) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "groups", "users"
 end
