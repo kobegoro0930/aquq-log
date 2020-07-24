976e780bfaf8ef4fab01a0554f6827f1c0da9a56
kobegoro0930 <62911575+kobegoro@users.noreply.github.com>
2020-07-24 14:22:53 +0900

    Change model group

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,8 +1,8 @@
 class Group < ApplicationRecord
-  belongs_to :user
+  # belongs_to :user
   has_many :saves
-  has_many :groups_tags
-  has_many :tags, throgh: :groups_tags
+  # has_many :group_tags
+  # has_many :tags, throgh: :group_tags
   has_many :images
   has_many :comments
   has_many :wents
