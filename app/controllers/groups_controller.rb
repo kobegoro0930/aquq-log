2943e3c87bbf2bad56f90ea7ec2bf05c35a103c7
kobegoro0930 <62911575+kobegoro@users.noreply.github.com>
2020-07-24 14:23:33 +0900

    Edit controller, view group

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,7 +1,25 @@
 class GroupsController < ApplicationController
   def new
+    @group = Group.new
   end
 
   def create
+    @group = Group.new(group_params)
+    if @group.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def group_params
+    params.require(:group).permit(
+      :name,
+      :catchphrase,
+      :title,
+      :text,
+      :area,
+    )
   end
 end
