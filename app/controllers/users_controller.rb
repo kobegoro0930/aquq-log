86cb3003a6d10398b12fad8e763e31b5963d1de4
kobegoro0930 <62911575+kobegoro@users.noreply.github.com>
2020-08-12 22:58:23 +0900

    Resolve conflict

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,4 @@
 class UsersController < ApplicationController
-<<<<<<< HEAD
 
   before_action :set_comments
   
@@ -12,9 +11,6 @@ class UsersController < ApplicationController
 
   def set_comments
     @comments = Comment.all
-=======
-  def show
-    @user = User.find(params[:id])
->>>>>>> 451c64c3d2ced3849da3e97a9c5d0885dc6d4d8b
+
   end
 end
