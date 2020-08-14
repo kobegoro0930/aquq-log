cffe2d399750559dd253bba5a9d9cae0a3d37834
kobegoro0930 <62911575+kobegoro@users.noreply.github.com>
2020-08-14 22:29:15 +0900

    Fix image_uploder.rb

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -44,5 +44,5 @@ class ImageUploader < CarrierWave::Uploader::Base
   # def filename
   #   "something.jpg" if original_filename
   # end
-  process resize_to_fit: [100, 100]
+  process resize_to_fit: [600, 600]
 end
