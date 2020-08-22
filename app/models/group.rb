37d88975d6530cae3467e514d74580d58cea6f8b
kobegoro0930 <62911575+kobegoro@users.noreply.github.com>
2020-08-22 16:21:07 +0900

    Modificate_Routing&Model

--- app/models/group.rb
+++ app/models/group.rb
@@ -5,7 +5,11 @@ class Group < ApplicationRecord
   # has_many :tags, throgh: :group_tags
   has_many :images
   has_many :comments
-  has_many :wents
+  has_many :wents, dependent: :destroy
+  def went_user(user_id)
+    wents.find_by(user_id: user_id)
+  end
+
   has_many :images, dependent: :destroy
   accepts_nested_attributes_for :images, allow_destroy: true
 end
