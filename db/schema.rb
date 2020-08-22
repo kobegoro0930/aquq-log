cbd1d58514e3232da5a0886254545b158963637a
kobegoro0930 <62911575+kobegoro@users.noreply.github.com>
2020-08-22 16:18:30 +0900

    Create Model_Went

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_18_014307) do
+ActiveRecord::Schema.define(version: 2020_08_22_033159) do
 
   create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "group_id"
@@ -76,6 +76,15 @@ ActiveRecord::Schema.define(version: 2020_08_18_014307) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  create_table "wents", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "group_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["group_id"], name: "index_wents_on_group_id"
+    t.index ["user_id"], name: "index_wents_on_user_id"
+  end
+
   add_foreign_key "comments", "groups"
   add_foreign_key "comments", "users"
   add_foreign_key "group_tags", "groups"
@@ -83,4 +92,6 @@ ActiveRecord::Schema.define(version: 2020_08_18_014307) do
   add_foreign_key "images", "groups"
   add_foreign_key "tags", "groups"
   add_foreign_key "tags", "tags"
+  add_foreign_key "wents", "groups"
+  add_foreign_key "wents", "users"
 end
