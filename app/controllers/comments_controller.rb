22e2056183778eaa015fca353396354613aa2985
kobegoro0930 <62911575+kobegoro@users.noreply.github.com>
2020-09-07 09:50:38 +0900

    Fix-comments_controller
    コメント編集機能の修正

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -49,5 +49,10 @@ class CommentsController < ApplicationController
   def set_all
     @all_comments = Comment.all
     @all_groups = Group.all
+    @q = Group.ransack(params[:q])
+  end
+
+  def search_params
+    params.require(:q).permit(:area, :text)
   end
 end
