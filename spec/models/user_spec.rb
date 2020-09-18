df63e5d35b4903d98df9397bac02f5e3529d4ba9
kobegoro0930 <62911575+kobegoro@users.noreply.github.com>
2020-09-18 17:40:04 +0900

    Writing test code

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,7 +4,13 @@ describe User do
     it "nameがない場合は登録できないこと" do
       user = User.new(name: nil, email: "kkk@gmail.com", password: "00000000", password_confirmation: "00000000")
       user.valid?
-      expect(user.errors[:name]).to include("can't be blank")
+      expect(user.errors[:name]).to include("を入力してください")
+    end
+
+    it "emailがない場合は登録できないこと" do
+      user = User.new(name: "kimutaku", email: nil, password: "00000000", password_confirmation: "00000000")
+      user.valid?
+      expect(user.errors[:email]).to include("を入力してください")
     end
   end
 end
