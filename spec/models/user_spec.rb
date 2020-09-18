cdd54a89ded9fe833b7e61b68339ea3d1a3fbc8f
kobegoro0930 <62911575+kobegoro@users.noreply.github.com>
2020-09-18 18:12:48 +0900

    Setting FactoryBot

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,13 +2,13 @@ require 'rails_helper'
 describe User do
   describe '#create' do
     it "nameがない場合は登録できないこと" do
-      user = User.new(name: nil, email: "kkk@gmail.com", password: "00000000", password_confirmation: "00000000")
+      user = build(:user, name: nil)
       user.valid?
       expect(user.errors[:name]).to include("を入力してください")
     end
 
     it "emailがない場合は登録できないこと" do
-      user = User.new(name: "kimutaku", email: nil, password: "00000000", password_confirmation: "00000000")
+      user = build(:user, email: nil)
       user.valid?
       expect(user.errors[:email]).to include("を入力してください")
     end
