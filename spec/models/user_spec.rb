364bd455577f48069cfcffa7cb918b1e2ff52121
kobegoro0930 <62911575+kobegoro@users.noreply.github.com>
2020-09-21 13:49:36 +0900

    Writing test code for user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -14,10 +14,47 @@ describe User do
       expect(user.errors[:name]).to include("を入力してください")
     end
 
+    it "nameが10文字以下では登録できること " do
+      user = build(:user, name: "0123456789")
+      expect(user).to be_valid
+    end
+
+    it " nameが11文字以上であれば登録できないこと" do
+      user = build(:user, name: "01234567890")
+      user.valid?
+      expect(user.errors[:name]).to include("は10文字以内で入力してください")
+    end
+
     it "emailがない場合は登録できないこと" do
       user = build(:user, email: nil)
       user.valid?
       expect(user.errors[:email]).to include("を入力してください")
+    end
+
+    it "passwordがない場合は登録できないこと" do
+      user = build(:user, password: nil)
+      user.valid?
+      expect(user.errors[:password]).to include("を入力してください")
+    end
+
+    it "passwordが存在してもpassword_confirmationがない場合は登録できないこと" do
+      user = build(:user, password_confirmation: "")
+      user.valid?
+      expect(user.errors[:password_confirmation]).to include("とパスワードの入力が一致しません")
+    end
+
+    it "重複したnameが存在する場合登録できないこと" do
+      user = create(:user)
+      another_user = build(:user, name: user.name)
+      another_user.valid?
+      expect(another_user.errors[:name]).to include("はすでに存在します")
+    end
+
+    it "重複したemailが存在する場合登録できないこと" do
+      user = create(:user)
+      another_user = build(:user, email: user.email)
+      another_user.valid?
+      expect(another_user.errors[:email]).to include("はすでに存在します")
     end
 
   end
