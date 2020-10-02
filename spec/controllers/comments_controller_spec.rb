79fa3590926efef980529e525f7bad9cf80ae348
kobegoro0930 <62911575+kobegoro@users.noreply.github.com>
2020-10-02 15:49:16 +0900

    test-code

--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -3,6 +3,8 @@ require 'rails_helper'
 describe CommentsController do
   describe 'GET #new' do
     it "new.html.erbに遷移すること" do
+      get :new
+      expect(response).to render_template :new
     end
   end
 
